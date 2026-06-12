dc09364c5ce72f926577eded5ddc70c28c15b22c
shankar uranw <170225098+shankaruranw@users.noreply.github.com>
2026-06-12 15:13:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
-  define: {
-    'process.env.VITE_API_BASE_URL':JSON.stringify(process.env.VITE_API_BASE_URL ||'https://omnirecon-ai-1.onrender.com')
-  }
+  
 })
